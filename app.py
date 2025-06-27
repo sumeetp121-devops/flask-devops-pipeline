6ed07f23a7f40f89ce7414256d5a24d077cbc0c9
Sumeet Patel <sumeet.prac0811@gmail.com>
2025-06-27 06:31:28 +0000

    fixed python indentation

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-        return 'Hello, Sumeet! DevOps pipeline is working!'
+def hello():
+    return 'Hello from CI/CD pipeline!'
 
-    if __name__ == '__main__':
-            app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
